f24fe20c7fa20aabb1981796cb34987b3fd6da38
Alex <sanyaslovo@gmail.com>
2021-02-17 15:11:52 +0200

    first commit

---
+++ 1_Lesson/my-app/src/components/Container/Container.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './Container.css'
+class Container extends Component {
+    render() {
+        return (
+            <div className='container'>
+                container
+            </div>
+        );
+    }
+}
+
+export default Container;
